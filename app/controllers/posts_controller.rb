78d8a8ed3faa6aa8fbb803e5297a5a4b4738b3f9
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-14 16:07:48 +0900

    Completed the entire bog app with lot of copy paste

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -17,41 +18,39 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def edit
-    #grab the data from the database and input it into edittable fields
-    @posts = Post.new(post_params)
-
+    # find and grab the data from the database and input it into edittable fields
     # update the new data into the database
-
-
     # redirect the page to the show page, showing the newly updated information
-
-
+    #BUT IN REALITY YOU DONT NEED ANY OF THAT. YOU ONLY NEED THE @post = Post.find(params[:id]) SET IN THE set_post AND CALLED IN THE before_action ABOVE.
   end
 
   def update
-
+     #YOU ONLY NEED THE @post = Post.find(params[:id]) SET IN THE set_post AND CALLED IN THE before_action ABOVE.
   end
 
-  def delete
-
+  def destroy
+     #YOU ONLY NEED THE @post = Post.find(params[:id]) SET IN THE set_post AND CALLED IN THE before_action ABOVE.
+     @post.destroy
+     redirect_to "/posts/"
   end
 
 private
 
   def post_params
-    params.require(:post).permit(
-    :title,
-    :body,
-    :category
-    )
+    params.require(:post).permit(:title, :body, :category)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 
 end
