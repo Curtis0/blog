681df370b75733e7eed9a17d428a812d3d4547a1
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-27 00:16:00 +0900

    Created comments controller added belongs_to customer to comment model. edited bootstrap to look better in new.html.erb. created migration file for comments in terminal.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621033356) do
+ActiveRecord::Schema.define(version: 20150621054432) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20150621033356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "customer_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["customer_id"], name: "index_comments_on_customer_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
